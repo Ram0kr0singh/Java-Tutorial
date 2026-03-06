6df0526b696a2ec84227b6b31cfe4e7e1f869102
Ram Kumar Singh <ramkumar8920931924@gmail.com>
2026-03-07 01:35:01 +0530

    Update NestedIfs.java

--- src/NestedIfs.java
+++ src/NestedIfs.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class NestedIfs {
+    static void main(String[] args) {
+
+        boolean isStudent = true;
+        boolean isSenior = true;
+
+        double price = 9.99;
+
+        if (isStudent){
+            if (isSenior){
+                System.out.println("You get a discount of 50%");
+                price *= 0.5;
+            }
+            else {
+                System.out.println("You get a discount of 10%");
+                price *= 0.9;
+            }
+        }
+        else {
+            price *= 1;
+        }
+        System.out.printf("The price is : $%.2f", price);
+    }
 }
