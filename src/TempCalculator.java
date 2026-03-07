bda9e3a7d5ee954e0cafd077eac8f9040d0a3b7d
Ram Kumar Singh <ramkumar8920931924@gmail.com>
2026-03-08 02:11:14 +0530

    Temp Calc

--- src/TempCalculator.java
+++ src/TempCalculator.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class TempCalculator {
+    static void main(String[] args) {
+
+    //Temperature Calculator
+        System.out.print("Enter the Temperature: ");
+        double temp;
+        String unit;
+        double newTemp;
+
+        Scanner scanner = new Scanner(System.in);
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F): ");
+        unit = scanner.next().toUpperCase();
+
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5/9 : (temp * 9/5) + 32;
+        System.out.printf("%.1f%s",newTemp, unit);
+
+        scanner.close();
+    }
 }
