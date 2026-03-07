537056bc23e2091b3c45b3b5e76473a76742ac9a
Ram Kumar Singh <ramkumar8920931924@gmail.com>
2026-03-08 00:49:44 +0530

    Substring Method

--- src/SubstringProg.java
+++ src/SubstringProg.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SubstringProg {
+    public static void main(String[] args) {
+        System.out.print("Enter your email: ");
+         Scanner sc = new Scanner(System.in);
+         String email = sc.nextLine();
+
+        if (email.contains("@")) {
+//         String email = "Ramkumar@gmail.com";
+            String username = email.substring(0, email.indexOf("@"));
+            String domain = email.substring(email.indexOf("@") + 1);
+
+            System.out.println(username);
+            System.out.println(domain);
+        }
+        else{
+            System.out.println("Enter a proper email");
+
+        }
+
+        sc.close();
+    }
 }
