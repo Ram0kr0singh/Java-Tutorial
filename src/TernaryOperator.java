3cdcab60419803ae521cd0c6b6db237233585578
Ram Kumar Singh <ramkumar8920931924@gmail.com>
2026-03-08 01:49:41 +0530

    Ternary Operator

--- src/TernaryOperator.java
+++ src/TernaryOperator.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class TernaryOperator {
+    static void main(String[] args) {
+    // variable = (condition) ? ifTrue : ifFalse;
+
+        int num = 66;
+//
+//        String passOrFail = (num >= 60) ? "PASS" : "FAIL";
+//        System.out.println(passOrFail);
+
+//        EvenOdd
+
+        String evenOrOdd = (num % 2 == 0) ? "Even Number" : "Odd Number";
+        System.out.println(evenOrOdd);
+
+    }
 }
